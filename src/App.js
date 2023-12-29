5429b16710bb2f9963541e9b4b08a5a3c1cffec6
NafisaHassan0 <111084437+NafisaHassan0@users.noreply.github.com>
2023-12-29 17:11:09 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import './App.css';
-import Dashboard from './pages/Dashboard';
+// import './App.css';
+// import Dashboard from './pages/Dashboard';
 
+// function App() {
+//   return (
+//     <Dashboard />
+//   );
+// }
+
+// export default App;
+// import "./App.css";
+// import { Provider } from "react-redux";
+// import { store } from "./store/index";
+// import Dashboard from "./pages/Dashboard";
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//       <Dashboard />
+//     </Provider>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import SearchPage from "./pages/Search";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
 function App() {
   return (
-    <Dashboard />
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
