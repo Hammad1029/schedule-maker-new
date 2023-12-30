23c8f9c5adc18b7ae608822050a299474573b3b9
NafisaHassan0 <111084437+NafisaHassan0@users.noreply.github.com>
2023-12-31 04:20:04 +0500

    integrated search

--- src/components/Constraints.jsx
+++ src/components/Constraints.jsx
@@ -15,6 +15,7 @@ import {
 import { useState } from "react";
 import _ from "lodash";
 import CourseListAccordion from "./CourseListAccordion";
+
 const Constraints = ({ selectedCourses, slots }) => {
   const [prioritized, setPrioritized] = useState([]);
   const [prioritizedTeachers, setPrioritizedTeachers] = useState({});
